97f9d4f8dd924833fbb38af237740af2c0848644
pierre <pierre.gourgouillon@ynov.com>
2021-02-04 01:27:18 +0100

    in progress

---
+++ assets/file.js
@@ -0,0 +1,29 @@
+function randomArtist() {
+    var random = Math.floor(Math.random()*52);
+    document.location.href="http://localhost:8080/artist/"+random;
+}
+
+function displayDate(thingId) {
+    var targetElement;
+    targetElement = document.getElementById(thingId) ;
+    if (targetElement.style.display == "none")
+    {
+        targetElement.style.display = "" ;
+    } else {
+        targetElement.style.display = "none" ;
+    }
+}
+
+function petitTest() {
+    var valInput = document.getElementById('search').value;
+    var valData = document.getElementById('searchs').options;
+    for(i = 0; i < valData.length; i++) {
+        if(valData[i].value.toLowerCase() == valInput.toLowerCase()) {
+            var ID = valData[i].dataset.id
+            document.location.href="http://localhost:8080/artist/"+ID;
+            return;
+        }
+    }
+    document.location.href="http://localhost:8080/error/";
+}
+
